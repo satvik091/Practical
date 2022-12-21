1697ab31830ed36f558f4a9bed6ebed5141ed701
Harshit Singh Rathore <harsh.rathore.2303@gmail.com>
2022-12-21 18:16:26 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int m, n;
+	scanf ("%d %d", &m, &n);
+	
+    int a[m][n];
+	for (int i=0; i<n; i++)
+	for (int j=0; j<m; j++)
+	scanf ("%d", &a[i][j]);
+	
+	for (int i=0; i<n; i++)
+	{
+		for (int j=0; j<m; j++)
+		{
+			if (i==j)
+			printf ("%d ", a[i][j]);
+		}
+	}
+	
+	for (int i=0; i<n; i++)
+	for (int j=0; j<m; j++)
+    if ((i+j)==(n-1))
+    printf ("%d ", a[i][j]);
+    return 0;   
+}    
